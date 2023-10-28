732536772660ef733be8393aaf1aee027ce730b7
SheikhAfham-1993 <56600934+SheikhAfham-1993@users.noreply.github.com>
2023-10-28 18:17:59 +0200

    Fixed IRepo bug

--- store/global.tsx
+++ store/global.tsx
@@ -1,22 +1,16 @@
-import { IOwner, IUserData } from '@/Interface/IOwner'
-import { IRepo } from '@/Interface/IRepo'
+import { IUserData } from '@/Interface/IOwner'
+
 import { create } from 'zustand'
 
 type Repository = {
   userData: IUserData | undefined
-  ownerData: IOwner | undefined
-  repoData: IRepo[]
   setUserData: (userData: IUserData | undefined) => void
-  setOwnerData: (ownerData: IOwner | undefined) => void
-  setRepoData: (repoData: IRepo[]) => void
 }
 let useStore = create<Repository>((set) => ({
   userData: undefined,
   ownerData: undefined,
   repoData: [],
   setUserData: (userData: IUserData | undefined) => set({ userData }),
-  setOwnerData: (ownerData: IOwner | undefined) => set({ ownerData }),
-  setRepoData: (repoData: IRepo[]) => set({ repoData }),
 }))
 
 export default useStore
